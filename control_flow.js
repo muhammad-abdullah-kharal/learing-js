84829a612230d8768a4a9f6f4b5e2594a72e0ce4
Muhammad Abdullah <raiabdkharal@gmail.com>
2025-10-19 15:43:15 +0500

    this is for of loop

--- control_flow.js
+++ control_flow.js
@@ -11,3 +11,62 @@ const isuserlogged=true;
 
 // const amount = 5000;
 // if (amount>1000)console.log("you have more money "),(console.log("i think it would be not good"));
+
+
+//                                            truthy values in js
+
+// const user="ab@gmail.com";
+// if(user){
+//     console.log("user has value")
+// }else{
+//     console.log("user has no value")
+// }
+
+
+
+
+
+        //                                      for of loop
+
+        // const number=[1,3,4,5,6,7,8];
+
+        // for (const item of number) {
+        //     console.log(item);
+
+        // }
+
+        // const str="hello world";
+
+        // for (const char of str) {
+        //     if(char===' '){
+        //         continue;
+        //     }
+        //     console.log(char);  
+        // }
+
+
+
+
+
+        //     Map
+
+
+        // const map =new Map();
+        // map.set("Pk","Pakistan")
+        // map.set("Pa","Phalistine")
+
+        // for (const [key,values] of map) {
+        //     console.log(key + "  :  " + values);
+        // }
+
+        const myobj={
+            name:"rayan",
+            age:22,
+            hobby:"coding"
+        }
+
+        //               this not work because objects are not iterable
+        for (const [key,value] of object) {
+                        console.log(key + "  :  " + values);
+
+        }
