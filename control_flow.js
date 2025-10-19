d539f8c0cb9b110ce434571a38749eaf46ac7f98
Muhammad Abdullah <raiabdkharal@gmail.com>
2025-10-19 14:34:40 +0500

    learning condition if else

---
+++ control_flow.js
@@ -0,0 +1,13 @@
+const isuserlogged=true;
+
+
+
+// if (2=="2"){
+//     console.log("executed")
+// }
+// if (2==="2"){
+//     console.log("executed")
+// }
+
+// const amount = 5000;
+// if (amount>1000)console.log("you have more money "),(console.log("i think it would be not good"));
