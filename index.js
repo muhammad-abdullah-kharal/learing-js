e9074675401833849eea48234ea3cf34bd4523b4
Muhammad Abdullah <raiabdkharal@gmail.com>
2025-10-19 12:50:41 +0500

    first push t github to the in middel of learing

---
+++ index.js
@@ -0,0 +1,111 @@
+// console.log("this is first program")
+// const a=67;
+// a="harry";
+// console.log(a);
+
+// const hel = {
+//     heel: "kmfkm",
+// };
+// let a34 =5+5
+// console.log(a34)
+
+// if(a34<a){
+//     console.log(true)
+// }
+
+// let g=2;
+
+// if(g<2){
+// console.log(2)
+
+// }else if (g<3){
+// console.log(3)
+
+// }else{
+//     console.log("h")
+// }
+
+
+// function g(){
+// console.log("hellleo kcms  kllmlkm");
+// return "hi"
+
+// }
+// console.log(g())
+// console.log(g())
+
+// let age =prompt("enter your age ");
+// age=Number.parseInt(age);
+
+
+// candrive = (age)=>{
+
+//    return age>18? true:false
+
+// }
+
+// if(candrive()){
+//     alert("You can drive")
+// }
+// else{
+//     alert("you cant drive")
+// }
+
+
+
+// let runAgain= true;
+
+// while(runAgain){
+    
+//     let age =prompt("enter your age ");
+//     age=Number.parseInt(age);
+
+//     if(age<0){
+//         console.error("enter valid age ")
+//         break;
+//     }
+    
+    
+//     candrive = (age)=>{
+        
+//         return age>18? true:false
+        
+//     }
+    
+//     if(candrive(age)){
+//         alert("You can drive")
+//     }
+//     else{
+//         alert("you cant drive")
+//     }
+//     runAgain=confirm("you want again")
+
+// }
+
+
+// let number =prompt("enter tour number");
+
+// number =Number.parseInt(number);
+
+// if(number>4){
+//     location.href="http:google.com"
+// }
+
+// let color=prompt("enter the color")
+//  document.body.style.background=color
+// console.log("jksfkjf nflk");
+
+// console.log("rayan")
+
+
+
+// const myobj={
+//     name:"ABdullah",
+//     section:'A',
+// };
+
+// const {name:n}=myobj;
+
+// console.log(n);
+
+
